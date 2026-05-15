6b7b47859ec4a6b4ced6af972a6f9ba2a237683e
BENOUNENE Abderrahmene <77858997+abderrahmenex86@users.noreply.github.com>
2026-05-15 20:38:06 +0100

    Update training script for multiclass segmentation

--- train.py
+++ train.py
@@ -16,10 +16,10 @@ if __name__ == "__main__":
         ToImage,
     )
 
-    from BCEDiceLoss import BCEDiceLoss
-    from datasets import PlantSegDataset
+    from dataset import PlantSegDataset
     from helpers import train
-    from models import DiseaseSegmenter
+    from loss import MulticlassDiceLoss
+    from model import DiseaseSegmenter
 
     random.seed(1337)
     np.random.seed(1337)
@@ -28,18 +28,19 @@ if __name__ == "__main__":
     torch.backends.cudnn.benchmark = True
 
     n_epochs = 40
-    batch_size = 16
+    batch_size = 8
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     train_workers = 16
-    val_workers = 12
+    val_workers = 4
     pin_memory = True
     prefetch_factor = 4
+    persistent_workers = True
 
-    criterion = BCEDiceLoss(pos_weight=torch.tensor([10.0])).to(device)
+    n_classes = 116
+    criterion = MulticlassDiceLoss(num_classes=n_classes).to(device)
 
     root_dir = "dataset/plantsegv3"
-    n_classes = 2
 
     train_transforms = Compose(
         [
@@ -62,12 +63,13 @@ if __name__ == "__main__":
         num_workers=train_workers,
         pin_memory=pin_memory,
         prefetch_factor=prefetch_factor,
+        persistent_workers=persistent_workers,
     )
 
     val_transforms = Compose(
         [
             ToImage(),
-            Resize((512, 512)),
+            Resize((520, 520)),
             ToDtype(torch.float32, scale=True),
             Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ]
@@ -82,9 +84,10 @@ if __name__ == "__main__":
         num_workers=val_workers,
         pin_memory=pin_memory,
         prefetch_factor=prefetch_factor,
+        persistent_workers=persistent_workers,
     )
 
-    model = DiseaseSegmenter().to(device)
+    model = DiseaseSegmenter(num_classes=n_classes).to(device)
 
     optimizer = torch.optim.AdamW(
         [
